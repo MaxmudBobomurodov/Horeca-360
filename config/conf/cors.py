6ff97195c921ce5d520dd52235ee13e56e34a879
Maxmud <maxmudbobomurodov151@gmail.com>
2025-10-12 03:13:35 +0500

    s

--- config/conf/cors.py
+++ config/conf/cors.py
@@ -1,13 +1,18 @@
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+    'https://horeca.felixits.uz',
+    'http://91.218.246.233',
+    'http://91.218.246.233:8000',
+    'http://91.218.246.233:8001',
+]
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
     'https://agro365.vercel.app',
     'https://agro365-admin.vercel.app',
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:8080',
-    'http://127.0.0.1:8080',
-    'https://horeca.felixits.uz',
+    'http://91.218.246.233:8000',
+    'http://91.218.246.233:8001',
 ]
